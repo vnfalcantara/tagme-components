97caf6ea95e18892e98f4c59e4cb74631ec841c4
Vinicius Alcantara <nitcho@Viniciuss-Macintosh.local>
2020-09-30 16:06:49 -0300

    button component

--- projects/tagme-components-example/src/app/app.module.ts
+++ projects/tagme-components-example/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// COMPONENTS
+import { ButtonModule } from 'tagme-components'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/tagme-components/src/public-api.ts
+++ projects/tagme-components/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/tagme-components.service';
 export * from './lib/tagme-components.component';
 export * from './lib/tagme-components.module';
+
+export * from './lib/button/button.component';
+export * from './lib/button/button.module';
